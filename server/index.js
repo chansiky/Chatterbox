e1c49379937b7dc495ca6b71efc61af1951e2db5
chansiky <chansiky@gmail.com>
2018-08-17 23:52:29 -0500

    heroku deployment

--- server/index.js
+++ server/index.js
@@ -104,8 +104,7 @@ const createApp = () => {
 
 const startListening = () => {
   // start listening (and create a 'server' object representing our server)
-  const server = app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
-
+  const server = app.listen(process.env.port||4444, () => console.log(`Mixing it up on port ${PORT}`))
 /*
   let httpsOptions = {
   };
