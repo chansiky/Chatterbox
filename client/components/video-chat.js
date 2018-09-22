e667e3a6200d78518418b26df18bccc4eb3db015
chansiky <chansiky@gmail.com>
2018-09-22 08:58:01 -0600

    debugging heroku branch

--- client/components/video-chat.js
+++ client/components/video-chat.js
@@ -89,7 +89,7 @@ class VideoChat extends React.Component {
 
   // Open and configure the connection to the WebSocket server.
   connect() {
-    console.log('tying to connect")
+    console.log("tying to connect")
     let serverUrl;
     let scheme = 'ws';
 
